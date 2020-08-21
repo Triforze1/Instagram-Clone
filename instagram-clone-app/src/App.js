9e31cd522df0d97213dae7028d2a42c3bf8503c9
Triforze1 <b_farrill@ymail.com>
2020-08-20 21:48:00 -0400

    Created Header (Start)

--- instagram-clone-app/src/App.js
+++ instagram-clone-app/src/App.js
@@ -3,8 +3,17 @@ import './css/App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <div className="app-header">
+        <img className="app-headerImage" 
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" 
+          alt=""/>
+      </div>
+
       <h1>Hello Clever Programmer</h1>
+
+      {/* Posts */}
+      {/* Posts */}
     </div>
   );
 }
